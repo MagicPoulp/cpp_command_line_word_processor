3eb383b433a36380744dd314e55d7ea5a6eed570
Thierry Vilmart <thierry.vilmart@REPLACE.com>
2023-12-17 08:38:29 +0100

    relocate the ordering of processorSteps to the build function

--- src/ProcessorBuilder.cpp
+++ src/ProcessorBuilder.cpp
@@ -1,9 +1,15 @@
+#include <algorithm>
 #include "ProcessorBuilder.hpp"
 #include "Processor.hpp"
 #include "Enums.hpp"
 
 // in the Command design pattern, we specify first what we will do
 void ProcessorBuilder::sort(sort_dir direction) {
+  std::vector<processor_step>::iterator itSortStep = std::find_if(transformSteps.begin(), transformSteps.end(),
+                                  [](processor_step step) { return step == SORT_ASC || step == SORT_DESC; });
+  if (itSortStep != transformSteps.end()) {
+    transformSteps.erase(itSortStep);
+  }
   switch(direction) {
   case ASC:
     transformSteps.push_back(SORT_ASC);
@@ -19,5 +25,11 @@ void ProcessorBuilder::distinct() {
 }
 
 Processor ProcessorBuilder::build() {
+  // having the sorting step before makes distinct faster
+  std::vector<processor_step>::iterator itSortStep = std::find_if(transformSteps.begin(), transformSteps.end(),
+                                                       [](processor_step step) { return step == SORT_ASC || step == SORT_DESC; });
+  if (itSortStep != transformSteps.end()) {
+    std::iter_swap(transformSteps.begin(), itSortStep);
+  }
   return Processor(transformSteps);
 }
--- src/ProcessorFactory.cpp
+++ src/ProcessorFactory.cpp
@@ -5,13 +5,12 @@
 
 Processor ProcessorFactory::create(bool sortWordSize, sort_dir sortingDirection, bool filterDuplicates) {
   ProcessorBuilder result;
+  if (filterDuplicates) {
+    result.distinct();
+  }
   if (sortWordSize) {
     // 2 parameters become one algo step
     result.sort(sortingDirection);
-  }
-  // having the sorting stepbefore makes distinct faster
-  if (filterDuplicates) {
-    result.distinct();
   }
   return result.build();
 }
